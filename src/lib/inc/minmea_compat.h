e0467ff77c2dd697a2f59a23a4413fca017d38c2
Mike Szczys <mike@golioth.io>
2023-02-18 02:15:45 -0600

    upload GPS data
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/lib/inc/minmea_compat.h
+++ src/lib/inc/minmea_compat.h
@@ -1,12 +1,8 @@
 #ifndef MINMEA_COMPAT_H_
 #define MINMEA_COMPAT_H_
 
-#include <zephyr/sys/timeutil.h>
-
-#define timegm timeutil_timegm
-
 #ifndef NAN
-#define NAN = 0.0/0.0
+#define NAN 0.0/0.0
 #endif
 
 #endif
