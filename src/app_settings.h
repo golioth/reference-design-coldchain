47ca642de4fde5887e304935085df653aa2ab3b3
Mike Szczys <mike@golioth.io>
2023-03-01 14:08:17 -0600

    add minimum delay between GPS readings
    This delay will be checked before storing the next GPS reading. If the
    minimum time has elapsed it will be stored, otherwise the reading will
    be discarded. This reading may end up happening 1 second later than
    expected because fo the 1 Hz NMEA data loop coming in from the GPS unit.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- src/app_settings.h
+++ src/app_settings.h
@@ -7,7 +7,11 @@
 #ifndef __APP_SETTINGS_H__
 #define __APP_SETTINGS_H__
 
+#include <stdint.h>
+#include <net/golioth/system_client.h>
+
 int32_t get_loop_delay_s(void);
+int32_t get_gps_delay_s(void);
 int app_register_settings(struct golioth_client *settings_client);
 
 #endif /* __APP_SETTINGS_H__ */
